51342f66512b1a8b689e5151f086e6fa919dd7d3
Damian Jadczak <62266703+djadczak956@users.noreply.github.com>
2021-12-22 08:02:15 -0500

    NumberGuess file

---
+++ NumberGuess.java
@@ -0,0 +1,23 @@
+// This program guesses two-digit numbers
+import java.util.*;
+
+public class NumberGuess {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int number = 42;    // Test the same number for now
+
+        System.out.print("Your guess? ---> ");
+        int guess = console.nextInt();
+        int numGuesses = 1;
+
+        while (guess != number) {
+            System.out.println("Incorrect.");
+            System.out.print("Your guess? ---> ");
+            guess = console.nextInt();
+            numGuesses++;
+        }
+
+        System.out.println("You got it right in " + numGuesses + " tries. ");
+        console.close();
+    }
+}
