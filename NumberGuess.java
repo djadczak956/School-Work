51275d8b9e10199e576a10335704a4ef7a28c0e9
Damian <jadczakdamian@gmail.com>
2021-12-30 16:16:30 -0500

    Created matches method that reports number of matching digits

--- NumberGuess.java
+++ NumberGuess.java
@@ -4,16 +4,20 @@ import java.util.*;
 public class NumberGuess {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        System.out.println("Try to get the two-digit number (1-99)");
 
         Random rand = new Random();
-        int number = rand.nextInt(100);    // Picks a random number from 1-99
+        int number = rand.nextInt(100);    // Picks a random number from 1-99 inclusives
 
+        // First guess
         System.out.print("Your guess? ---> ");
         int guess = console.nextInt();
         int numGuesses = 1;
 
+        // Give hints until correct guess is reached
         while (guess != number) {   // Loop through until the guess is true
-            System.out.println("Incorrect.");
+            int numMatches = matches(number, guess); 
+            System.out.println("Incorrect (hint: " + numMatches + " digits match)");
             System.out.print("Your guess? ---> ");
             guess = console.nextInt();
             numGuesses++;
@@ -21,5 +25,23 @@ public class NumberGuess {
 
         System.out.println("You got it right in " + numGuesses + " tries. ");
         console.close();
+    }
+
+    // This method checks how many digits match and reports it
+    public static int matches(int number, int guess) {
+        int numMatches = 0;
+
+        // Checks if the first digit of guess is equal to either of the digits of the number
+        if (guess / 10 == number / 10 || guess / 10 == number % 10) {
+            numMatches++; 
+        }
+
+        // Checks if the second digit of guess is equal to either of the digits of the number
+        // Taking into consideration the case if both digits are the same
+        if (guess / 10 != guess % 10 && (guess % 10 == number / 10 || guess % 10 == number % 10)){
+            numMatches++;
+        }
+        
+        return numMatches;
     }
 }
