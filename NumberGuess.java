9079ca610bdc47b12ec894553a90ded9285129d2
Damian <jadczakdamian@gmail.com>
2021-12-30 15:37:09 -0500

    Made a Random object for the number to guess

--- NumberGuess.java
+++ NumberGuess.java
@@ -1,16 +1,18 @@
-// This program guesses two-digit numbers
+// This program guesses two-digit numbers (0-99)
 import java.util.*;
 
 public class NumberGuess {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int number = 42;    // Test the same number for now
+
+        Random rand = new Random();
+        int number = rand.nextInt(100);    // Picks a random number from 1-99
 
         System.out.print("Your guess? ---> ");
         int guess = console.nextInt();
         int numGuesses = 1;
 
-        while (guess != number) {
+        while (guess != number) {   // Loop through until the guess is true
             System.out.println("Incorrect.");
             System.out.print("Your guess? ---> ");
             guess = console.nextInt();
