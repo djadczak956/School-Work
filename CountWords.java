e647f23cb5be54bed92de8e142cf3250ec141c44
Damian <jadczakdamian@gmail.com>
2022-01-17 15:03:48 -0500

    Made the lineCount method to count lines

--- CountWords.java
+++ CountWords.java
@@ -3,15 +3,22 @@ import java.io.*;
 
 public class CountWords {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(getFile().canRead());
+        File f = getFile();
+        Scanner input = new Scanner(f);
+        
+        wordCount(input);
+        System.out.println(lineCount(input));
+        charCount(input);
     }
 
 
+    // Returns the File that will be used
     public static File getFile() {
         Scanner console = new Scanner(System.in);
         System.out.print("Enter in a file name --> ");
         File f = new File(console.nextLine());
 
+        // Tries again for file name
         while (!f.exists()) {
             System.out.println("Hm. That did not seem to work. Try again. ");
             System.out.print("Enter in a file name --> ");
@@ -22,16 +29,24 @@ public class CountWords {
     }
 
 
+    // Returns the word count 
     public static int wordCount(Scanner input) throws FileNotFoundException {
         return 1;
     }
 
 
+    // Returns the line count
     public static int lineCount(Scanner input) throws FileNotFoundException {
-        return 1;
+        int count = 0;
+        while (input.hasNextLine()) {
+            input.nextLine();   // Move the cursor forward
+            count++;
+        }
+        return count;
     }
 
 
+    // Returns the character count
     public static int charCount(Scanner input) throws FileNotFoundException {
         return 1;
     }
