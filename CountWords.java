36c3b4ac72220830bc8282851393be3051c0bea1
Damian <jadczakdamian@gmail.com>
2022-01-17 14:47:44 -0500

    Made getFile method to get file name

--- CountWords.java
+++ CountWords.java
@@ -3,18 +3,37 @@ import java.io.*;
 
 public class CountWords {
     public static void main(String[] args) {
-
+        getFile();
     }
 
-    public static int wordCount(Scanner input) {
+
+    public static File getFile() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter in a file name --> ");
+        File f = new File(console.nextLine());
+
+        while (!f.exists()) {
+            System.out.println("Hm. That did not seem to work. Try again. ");
+            System.out.print("Enter in a file name --> ");
+            f = new File(console.nextLine());
+        }
+
+        console.close();
+        return f;
+    }
+
+
+    public static int wordCount(Scanner input) throws FileNotFoundException {
         return 1;
     }
 
-    public static int lineCount(Scanner input) {
+
+    public static int lineCount(Scanner input) throws FileNotFoundException {
         return 1;
     }
 
-    public static int charCount(Scanner input) {
+
+    public static int charCount(Scanner input) throws FileNotFoundException {
         return 1;
     }
 }
