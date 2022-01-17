ba0638e74ba4bbbf144b0b53dfb8816fd557d571
Damian <jadczakdamian@gmail.com>
2022-01-17 14:00:42 -0500

    Initialized new project

---
+++ CountWords.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class CountWords {
+    public static void main(String[] args) {
+
+    }
+
+    public static int wordCount(Scanner input) {
+        return 1;
+    }
+
+    public static int lineCount(Scanner input) {
+        return 1;
+    }
+
+    public static int charCount(Scanner input) {
+        return 1;
+    }
+}
