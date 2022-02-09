4052420b198457d9b112c3a444116d7e50c788bc
Damian <jadczakdamian@gmail.com>
2022-02-09 08:07:36 -0500

    guess and intro started

--- Mastermind.java
+++ Mastermind.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Mastermind {
     public static void main(String[] args) {
         Random r = new Random();
+        Scanner console = new Scanner(System.in);
         int[] digits = new int[4];
         generateRandom(r, digits);
 
@@ -11,10 +12,19 @@ public class Mastermind {
         }
     }
 
+    // Uses random to generate digits 1-9 in the array
     public static void generateRandom(Random r, int[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = r.nextInt(10);
+            array[i] = r.nextInt(10);   
         }
+    }
+
+    public static void guess(Scanner console) {
+        
+    }
+
+    public static void intro() {
+        System.out.println("This is mastermind! ");
     }
 }
 
