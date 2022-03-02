deec8c8fdd47d1003803b6d824f2df232a3acf00
Damian <jadczakdamian@gmail.com>
2022-03-02 16:55:12 -0500

    Made basic constructors

--- RataionalNumber.java
+++ RataionalNumber.java
@@ -2,8 +2,17 @@ public class RataionalNumber {
     private int numerator;
     private int denominator;
 
-    // @TODO: Constuctor for 1/0
-    // @TODO: Constuctor that accepts two values: numerator and denominator
+    // Constructs an object that is 0/1
+    public RataionalNumber() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+    // Constructs an object with values of your choice
+    public RataionalNumber(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
     // @TODO: Accessor for denominator
     // @TODO: Accessor for numerator
     // @TODO: String representation for example: "5/3"
