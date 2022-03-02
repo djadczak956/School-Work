bb44b7849e82e772427d02ffa663e7d1a7016631
Damian <jadczakdamian@gmail.com>
2022-03-02 17:33:22 -0500

    Created simplify method nice

--- RationalNumber.java
+++ RationalNumber.java
@@ -12,6 +12,7 @@ public class RationalNumber {
     public RationalNumber(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
+        simplify();
     }
 
     // Accessor for denominator
@@ -31,4 +32,21 @@ public class RationalNumber {
         return Integer.toString(numerator) + "/" + Integer.toString(denominator);  // Executes when if statement is false
     }
     // @TODO: Keep values simplified
+    public void simplify() {
+        if (denominator == 0) {     // Gives error if denominator is zero
+            throw new ArithmeticException("You can't divide by zero!!");
+        }
+
+        if (numerator == denominator) {     // Checks if value is simply 1
+            numerator = 1;
+            denominator = 1;
+        }
+        
+        for (int x = 2; x <= 10; x++) {
+            while (numerator % x == 0 && denominator % x == 0) {    // Checks if both values are divisible by x from for loop
+                numerator = numerator / x;
+                denominator = denominator / x;
+            }
+        }
+    }
 }
