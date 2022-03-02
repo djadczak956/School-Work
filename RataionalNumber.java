5758a4be7d4e1d22597817d60da48607e96cb9e6
Damian <jadczakdamian@gmail.com>
2022-03-02 07:56:36 -0500

    Added todo stuff for RationalNumber

---
+++ RataionalNumber.java
@@ -0,0 +1,11 @@
+public class RataionalNumber {
+    private int numerator;
+    private int denominator;
+
+    // @TODO: Constuctor for 1/0
+    // @TODO: Constuctor that accepts two values: numerator and denominator
+    // @TODO: Accessor for denominator
+    // @TODO: Accessor for numerator
+    // @TODO: String representation for example: "5/3"
+    // @TODO: Keep values simplified
+}
