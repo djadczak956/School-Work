c40f6bdf3f23b7cc4e5222af6629fa8f9f2fefcf
Damian <jadczakdamian@gmail.com>
2022-04-08 08:29:33 -0400

    Forgot to make commits ooooops

--- ReverseFile.java
+++ ReverseFile.java
@@ -6,14 +6,26 @@ import java.io.*;       // For File
 public class ReverseFile {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(new File("sample.txt"));
+        //PrintStream output = new PrintStream(new File("reversed_string.txt"));
+        System.out.println(convertToList(input));
     }
 
+    // Uses Scanner to take all the words of a file and convert to a list
     public static ArrayList<String> convertToList(Scanner input) {
         ArrayList<String> list = new ArrayList<String>();
         while (input.hasNext()) {
             list.add(input.next());
         }
-
         return list;
+    }
+
+    // I could just easily print out the array into the file in reverse order 
+    // but I am going to return a reversed array for giggles
+    public static ArrayList<String> reverseArray(ArrayList<String> list) {
+        ArrayList<String> reversed = new ArrayList<String>();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            reversed.add(list.get(i));
+        }
+        return reversed;
     }
 }
