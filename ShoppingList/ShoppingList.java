be321d77a73e64bc557058b5db14f773a09d3b15
Damian <jadczakdamian@gmail.com>
2022-04-09 21:19:12 -0400

    Constructor

---
+++ ShoppingList/ShoppingList.java
@@ -0,0 +1,13 @@
+package ShoppingList;
+
+import java.util.ArrayList;
+
+public class ShoppingList {
+    private ArrayList<ItemOrders> list;
+
+    // Constructs an empty list for ItemOrders
+    public ShoppingList() {
+        this.list = new ArrayList<ItemOrders>();
+    }
+
+}
