97461e69a0571b809bd6f8809eef8ce48e6cfc57
Damian <jadczakdamian@gmail.com>
2022-04-09 21:11:00 -0400

    Formatting hahaha

--- ShoppingList/ItemOrders.java
+++ ShoppingList/ItemOrders.java
@@ -4,8 +4,7 @@ public class ItemOrders {
     private int amount;
     private Item item;
 
-    // Constructs an object that accepts an Item object and how 
-    // many will be purchased
+    // Constructs an object that accepts an Item object and how many will be purchased
     public ItemOrders(Item item, int amount) {
         this.item = item;
         this.amount = amount;
