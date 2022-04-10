6cb715c599d05d28129ba38654b273775baabc2d
Damian <jadczakdamian@gmail.com>
2022-04-09 21:01:01 -0400

    Constructor for ItemOrders

---
+++ ShoppingList/ItemOrders.java
@@ -0,0 +1,13 @@
+package ShoppingList;
+
+public class ItemOrders {
+    private int amount;
+    private Item item;
+
+    // Constructs an object that accepts an Item object and how 
+    // many will be purchased
+    public ItemOrders(Item item, int amount) {
+        this.item = item;
+        this.amount = amount;
+    }
+}
