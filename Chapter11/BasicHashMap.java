b705c4a364319f425e66ef8377f2f46d231582ef
Damian <jadczakdamian@gmail.com>
2022-05-17 13:42:00 -0400

    Basic HashMap

---
+++ Chapter11/BasicHashMap.java
@@ -0,0 +1,10 @@
+package Chapter11;
+
+import java.util.*;
+
+public class BasicHashMap {
+    public static void main(String[] args) {
+        Map<String, Double> salaryMap = new HashMap();
+        salaryMap.put("Damian Jadczak", 3.14);
+    }
+}
