5f0fd9e892a401fed8b6af87702a8b3811f0fefe
Damian <jadczakdamian@gmail.com>
2022-05-23 12:59:35 -0400

    Started the project which will house methods

---
+++ Chapter11/Methods.java
@@ -0,0 +1,14 @@
+package Chapter11;
+
+import java.util.*;
+
+public class Methods {
+    public static void main(String[] args) {
+        List<Integer> part1 = new ArrayList<>();
+        List<Integer> part2 = new ArrayList<>();
+    }
+
+    public List alternate(List<E> first, List<E> second) {
+        
+    }
+}
