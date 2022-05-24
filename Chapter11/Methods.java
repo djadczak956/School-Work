21096f3536c840f5d2fa3ae265b94c0f1136b6a0
Damian <jadczakdamian@gmail.com>
2022-05-24 07:35:53 -0400

    alternate method works for lists of same size

--- Chapter11/Methods.java
+++ Chapter11/Methods.java
@@ -4,13 +4,32 @@ import java.util.*;
 
 public class Methods {
     public static void main(String[] args) {
-        List<Integer> part1 = new ArrayList<>();
-        List<Integer> part2 = new ArrayList<>();
+        List<Integer> part1 = new LinkedList<Integer>();
+        for (int i = 1; i <= 10; i++) {
+            part1.add(i);
+        }
+
+        List<Integer> part2 = new LinkedList<Integer>();
+        for (int i = 11; i <= 20; i++) {
+            part2.add(i);
+        }
+
+        System.out.println(alternate(part1, part2).toString());
     }
 
-    public List alternate(List<E> first, List<E> second) {
+    public static List alternate(List<Integer> first, List<Integer> second) {
+        List<Integer> newList = new ArrayList<>();
+        
         if (first.size() == second.size()) {
-            
+            Iterator<Integer> itr1 = first.iterator();
+            Iterator<Integer> itr2 = second.iterator();
+
+            while(itr1.hasNext()) {
+                newList.add(itr1.next());                
+                newList.add(itr2.next());                
+            }
         }
+
+        return newList;
     }
 }
