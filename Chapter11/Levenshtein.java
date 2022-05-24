d8d0adffee930bc3916145594cf351138a8e65f1
Damian <jadczakdamian@gmail.com>
2022-05-24 08:20:43 -0400

    Initial commit

---
+++ Chapter11/Levenshtein.java
@@ -0,0 +1,12 @@
+// Info on Levenshtein distance: https://en.wikipedia.org/wiki/Levenshtein_distance
+// This project may take some time
+
+package Chapter11;
+
+
+
+public class Levenshtein {
+    public static void main(String[] args) {
+
+    }
+}
