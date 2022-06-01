f76bdb5c6ffbd2d203e981942149553ee746b924
Damian <jadczakdamian@gmail.com>
2022-06-01 07:44:08 -0400

    Simple recursive method to reverse input

---
+++ Chapter12/Reverse.java
@@ -0,0 +1,21 @@
+package Chapter12;
+
+import java.util.*;
+import java.io.*;
+
+public class Reverse {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new File("Chapter12/sample.txt"));
+        reverse(input);
+        input.close();
+    }
+
+    // Simple method to reverse input
+    public static void reverse(Scanner input) {
+        while(input.hasNextLine()) {
+            String line = input.nextLine();
+            reverse(input);
+            System.out.println(line);
+        }
+    }
+}
