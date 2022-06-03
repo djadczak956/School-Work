b76aebb80f75ef99a01006547e16ccc382163607
Damian <jadczakdamian@gmail.com>
2022-06-03 07:36:59 -0400

    Recursive Euclid's algorithm

--- Chapter12/GCD.java
+++ Chapter12/GCD.java
@@ -1,9 +1,18 @@
 public class GCD {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(gcd(5, 25));
     }
 
     public static int gcd(int x, int y) {
-        return 1;
+        if (x < 0 || y < 0) {
+            // Handle negatives
+            return gcd(Math.abs(x), Math.abs(y));
+        } else if (y == 0) {
+            // Base case
+            return x;
+        } else {
+            // Recursive case
+            return gcd(y, x % y);
+        }
     }
 }
