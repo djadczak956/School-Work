21d07ce78a6f59190b9c70ade86e5d0b1e88598f
Damian <jadczakdamian@gmail.com>
2022-06-08 07:52:47 -0400

    Method to get a midpoint on a side of triangle

--- Chapter12/Sierpinski.java
+++ Chapter12/Sierpinski.java
@@ -4,5 +4,9 @@ public class Sierpinski {
     public static void main(String[] args) {
 
     }    
-    
+
+    // Method to calculate midpoint
+    public static Point midpoint(Point p1, Point p2) {
+        return new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+    }
 }
